9335a87f7929c5a2c3041ed189b517ff418ebb4e
Mário Guimarães <marioguima@hotmail.com>
2024-09-24 22:32:14 -0300

    [Refactor] Jump line

--- setup-vps-debian.sh
+++ setup-vps-debian.sh
@@ -5,6 +5,7 @@
 ##########
 
 echo "Passo 1: Atualizando repositórios e instalando dependências necessárias..."
+echo ""
 apt-get update
 apt install -y sudo gnupg2 wget ca-certificates apt-transport-https curl gnupg nano htop
 
@@ -14,6 +15,7 @@ else
     echo "Erro no Passo 1: Falha ao atualizar repositórios e instalar dependências."
     exit 1
 fi
+echo ""
 
 # Passo 2: Docker GPG key
 echo "Passo 2: Adicionando chave GPG do Docker..."
@@ -27,6 +29,7 @@ else
     echo "Erro no Passo 2: Falha ao adicionar a chave GPG do Docker."
     exit 1
 fi
+echo ""
 
 # Passo 3: Docker Apt sources
 echo "Passo 3: Configurando os repositórios do Docker..."
@@ -42,6 +45,7 @@ else
     echo "Erro no Passo 3: Falha ao configurar os repositórios do Docker."
     exit 1
 fi
+echo ""
 
 echo "Passo 4: Instalando o Docker..."
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -52,6 +56,7 @@ else
     echo "Erro no Passo 4: Falha ao instalar o Docker."
     exit 1
 fi
+echo ""
 
 # Passo 5: Configurar o docker para iniciar automaticamente
 echo "Passo 5: Configurando o Docker para iniciar automaticamente com o sistema..."
@@ -64,7 +69,7 @@ else
     echo "Erro no Passo 5: Falha ao configurar o Docker para iniciar automaticamente."
     exit 1
 fi
-
+echo ""
 
 ##################
 # Inicia o Swarm #
@@ -78,18 +83,20 @@ if [ -z "$IP_ADDR" ]; then
     echo "Erro no Passo 6: Não foi possível obter o IP da máquina."
     exit 1
 else
-    echo "IP da máquina obtido: $IP_ADDR"
+    echo "Passo 6 concluído com sucesso... IP da máquina obtido: $IP_ADDR"
 fi
+echo ""
 
 # Passo 7: Inicializa o Docker Swarm com o IP obtido
 echo "Passo 7: Inicializando o Docker Swarm..."
 docker swarm init --advertise-addr=$IP_ADDR
 
 if [ $? -eq 0 ]; then
-    echo "Docker Swarm inicializado com sucesso."
+    echo "Passo 7 concluído... Docker Swarm inicializado com sucesso."
 else
     echo "Erro no Passo 7: Falha ao inicializar o Docker Swarm."
     exit 1
 fi
+echo ""
 
 echo "Script executado com sucesso!"
